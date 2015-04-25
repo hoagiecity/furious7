48a4045df93813fd55f3575a8495cfeae26b888b
Zack Elliott <hubridnoxx@gmail.com>
2015-04-25 11:33:30 -0400

    Added ngCordova and camera support

--- furious7/www/js/app.js
+++ furious7/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ionic-service-core'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ionic.service.core', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
